5a778f33e4b09c976611e48a5e076d8b6a7e7cc9
Planeptunia <sergienkov03@gmail.com>
2024-08-22 21:38:50 +0300

    Powerbox fix
    Now with ability to open

--- Scripts/powerbox.gd
+++ Scripts/powerbox.gd
@@ -3,15 +3,19 @@ extends Node3D
 @onready var anim_player: AnimationPlayer = $"../AnimationPlayer"
 
 var opened: bool = false
-
+var closing: bool = false
 
 func interact():
 	if not opened:
 		anim_player.play("OpenPowerBox")
 	await anim_player.animation_finished
-	opened = true
+	if not closing:
+		opened = true
+	else: 
+		closing = false
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_released("interact"):
+	if Input.is_action_just_released("interact") and not opened:
 		anim_player.pause()
 		anim_player.play_backwards("OpenPowerBox")
+		closing = true
